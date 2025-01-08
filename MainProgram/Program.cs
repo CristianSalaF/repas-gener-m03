b38c5a6f9c628976d73cb02fc94ea05fcf2ea330
Cristian Sala <cristian.sala.7e8@itb.cat>
2025-01-08 19:53:12 +0100

    Feature: exercise2 and call in MainProgram project from #3

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace MainProgram
+﻿using MyLibrary;
+
+namespace MainProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Introdueix els costats A, B, i l'alçada d'un triangle: ");
+            Console.WriteLine(MyLibrary.Math.AreaToTriangle(float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine())));
         }
     }
 }
